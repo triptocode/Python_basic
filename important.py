68b3da56bc7fcb97bcf4c288197dda6e49a5554a
triptocode <3580rio@gmail.com>
2023-07-14 13:42:15 +0900

    python - basic

--- important.py
+++ important.py
@@ -1,45 +1,4 @@
-# print((3==3) and (5!=3)) # True
-
-# print(3=>3) # SyntaxError
-
-# if 4<3:
-#     print("when conditional is True, this sentence is printed!!!") 
-                         #  아무결과도 출력안됨 [Done] exited with code=0 in 0.078 seconds
-                         #  (조건이 True 일때만 실행하는데, 참이 아니라서)
-
-# if 4<3:
-#     print("when conditional is True, this sentence is printed!!!") 
-# else:
-#     print("when conditional is False, this sentence is printed!!!")
-
-
-# if True:
-#     print(1)
-#     print(2)
-# else:
-#     print(3)
-# print(4)
-
-
-
-if True:
-    if False:
-        print(1)
-        print(2)
-    else:
-        print(3)
-else: 
-    print(4)
-print(5)  # 3과 5가 한줄에 하나씩 출력 
-
-
-
-# greeting = input("hi입력:" )
-# print(greeting*2)
-
-
-
-#127
+##############################################  127   ##############################################
 
 resident_full_num= input("주민번호: ")
 second_num = resident_full_num.split('-')[1]
